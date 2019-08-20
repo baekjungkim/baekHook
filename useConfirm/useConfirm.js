8ac76ae951f57f983149e2cf0e54cde160c61cbd
baekjung <zvgandam@gmail.com>
2019-08-20 18:11:29 +0900

    useConfirm Hook update

--- useConfirm/useConfirm.js
+++ useConfirm/useConfirm.js
@@ -1,9 +1,9 @@
 export const useConfirm = (message = "", onConfirm, onCancel) => {
-  if (!onConfirm || typeof onConfirm !== "function") {
+  if (onConfirm && typeof onConfirm !== "function") {
     return;
   }
 
-  if (!onCancel || typeof onCancel !== "function") {
+  if (onCancel && typeof onCancel !== "function") {
     return;
   }
 
